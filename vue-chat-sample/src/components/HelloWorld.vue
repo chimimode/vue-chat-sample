95aad16e63760440d6d81b2c344987a84e180005
lsj_0517 <lsj_0517@sbs.co.kr>
2021-01-10 03:18:36 +0900

    init commit

---
+++ vue-chat-sample/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <button v-on:click="chat" class="button is-rounded">채팅</button>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+    methods: {
+      chat: function() {
+        console.log(this);
+      },
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
